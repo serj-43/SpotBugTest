9a87c9a4fb7d9b8902dfa444bb94e1a98c738c58
serj-43 <serj-43@yandex.ru>
2022-05-01 20:03:28 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+        long amount = 1000_60;
+        boolean registered = true;
+        int expected = 300;
+        long actual = service.calculate(amount, registered);
 
-    service.calculate(amount, registered);
-  }
+        System.out.println("ОР: " + expected + " ФР: " + actual);
+    }
 }
 
